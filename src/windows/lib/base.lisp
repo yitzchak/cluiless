7d9ea850566af42d9e2a8b5b84086e5d6522dca6
Tarn Burton <twburton@gmail.com>
2020-01-23 20:37:06 -0500

    Remove dll extension

--- src/windows/lib/base.lisp
+++ src/windows/lib/base.lisp
@@ -1,10 +1,10 @@
 (in-package #:cluiless/windows)
 
 (cffi:define-foreign-library user32
-  (:win32 (:default "user32.dll")))
+  (:win32 (:default "user32")))
 
 (cffi:define-foreign-library kernel32
-  (:win32 (:default "kernel32.dll")))
+  (:win32 (:default "kernel32")))
 
 (cffi:defctype wstring (:string :encoding :utf-16))
 
