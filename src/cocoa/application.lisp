17fbf8a33638975e8c3b6aea36e276686dff988c
Tarn Burton <twburton@gmail.com>
2020-01-30 14:54:05 -0500

    Add basic Cocoa app support

--- src/cocoa/application.lisp
+++ src/cocoa/application.lisp
@@ -1,11 +1,19 @@
 (in-package :cluiless/cocoa)
 
 (defclass application (cluiless:application)
-  ())
+  ((handle
+     :accessor handle)))
 
 (cluiless:defbackend :cocoa cluiless/cocoa)
 
 (defmethod initialize-instance :before ((instance application) &rest initargs &key &allow-other-keys)
   (declare (ignore initargs))
-  (cluiless:load-backend-libraries 'objc 'cocoa 'app-kit 'foundation))
+  (cluiless:load-backend-libraries 'objc 'cocoa 'app-kit 'foundation)
+  (setf (handle instance) (objc-msg-send "NSApplication" "shared")))
+
+(defmethod cluiless:run ((instance application))
+  (trivial-main-thread:with-body-in-main-thread ()
+    (float-features:with-float-traps-masked t
+      (cluiless:activate instance)
+      (objc-msg-send (handle instance) "run" :void))))
 
--- src/cocoa/lib/objective-c.lisp
+++ src/cocoa/lib/objective-c.lisp
@@ -1,7 +1,66 @@
 (in-package :cluiless/cocoa)
 
-(cffi:defcfun (get-class "objc_getClass" :library objc) :pointer
+(cffi:defcfun ("objc_getClass" :library objc) :pointer
   (name :string))
 
-(cffi:defcfun (register-name "sel_registerName" :library objc) :pointer
+(cffi:defcfun ("sel_registerName" :library objc) :pointer
   (name :string))
+
+(cffi:define-foreign-type id ()
+  ()
+  (:actual-type :pointer)
+  (:simple-parser id))
+
+(defmethod cffi:translate-to-foreign (value (type id))
+  (declare (ignore type))
+  value)
+
+(defmethod cffi:translate-to-foreign ((value string) (type id))
+  (declare (ignore type))
+  (objc-get-class value))
+
+(defmethod cffi:translate-from-foreign (value (type id))
+  (declare (ignore type))
+  value)
+
+(cffi:define-foreign-type sel ()
+  ()
+  (:actual-type :pointer)
+  (:simple-parser sel))
+
+(defmethod cffi:translate-to-foreign (value (type sel))
+  (declare (ignore type))
+  value)
+
+(defmethod cffi:translate-to-foreign ((value string) (type sel))
+  (declare (ignore type))
+  (sel-register-name value))
+
+(defmacro objc-msg-send (instance selector &optional (retval 'id) &rest args)
+  (let ((qargs (loop
+                 for (ty val) on args by #'cddr
+                 collect `(quote ,ty)
+                 collect val)))
+    (cond
+      ((or (eq :double retval) (eq :float retval))
+        `(cffi:foreign-funcall ("objc_msgSend_fpret" :library objc)
+           id ,instance
+           sel ,selector
+           ,@qargs
+           ,retval))
+      ((and (listp retval) (eq :struct (first retval)))
+        (with-gensyms (struct)
+          `(cffi:with-foreign-object (,struct ',retval)
+             (cffi:foreign-funcall ("objc_msgSend_stret" :library objc)
+               :pointer ,struct
+               id ,instance
+               sel ,selector
+               ,@qargs
+               :void))))
+      (t
+        `(cffi:foreign-funcall ("objc_msgSend" :library objc)
+           id ,instance
+           sel ,selector
+           ,@qargs
+           ,retval)))))
+
