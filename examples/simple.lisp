8a3186ee56c35bc38fb5673c687c7b8a38d559c0
Tarn Burton <twburton@gmail.com>
2020-03-12 07:42:13 -0400

    Fix submenus

--- examples/simple.lisp
+++ examples/simple.lisp
@@ -9,15 +9,15 @@
     (cond
       ((cluiless:valid-site-p app :menu-bar)
         (cluiless:append-definitions
-          win :menu-bar
-          '(:type :menu :label "Simple" :children (:type :action :name :about))))
+          app :menu-bar
+          '(:type :menu :label "Simple" :children ((:type :action :name :about)))))
       ((cluiless:valid-site-p win :primary-menu)
         (cluiless:append-definitions win :primary-menu '(:type :action :name :about)))
       ((cluiless:valid-site-p win :menu-bar)
         (cluiless:append-definitions
           win :menu-bar
-          '(:type :menu :label "File" :children (:type :action :name :open))
-          '(:type :menu :label "Help" :children (:type :action :name :about)))))))
+          '(:type :menu :label "File" :children ((:type :action :name :open)))
+          '(:type :menu :label "Help" :children ((:type :action :name :about))))))))
 
 (defmethod cluiless:activate-action ((name (eql :about)) instance parameter)
   (declare (ignore instance parameter))
